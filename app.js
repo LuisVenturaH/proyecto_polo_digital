4a1cee406bfbfcfe9fc27bc6d794762c34bfaa11
LuisVenturaH <l_ventur@yahoo.com>
2023-11-09 18:34:07 +0100

    updated API MySQL project Polo Digital

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const connection = mysql.createConnection({ // Esyto conecta la base de datos qu
     database: "polo_digital" // Se Conecta a la base de datos que le asignemos. 
 });
 
-
 // Conectar con MySQL
 connection.connect(function(error) {
     if (error) { 
@@ -22,48 +21,48 @@ connection.connect(function(error) {
     console.log("Conectado a MySQL");
 });
 
-// <<===== --------------------  INICIA Endpoint para INDEX ----------------------===>>>>>
+/* === >>> FUNCIONES UTILES */
+function handleSQLError(response, error, result, callback) {
+    if (error) {
+        response.static(400).send(`error ${error.messa}`);
+        return;
+    }
+    callback(result);
+}
 
-app.get('/carrusel', function(request, response) { 
-    connection.query('select * from eventos', function(error, result, fields) {
-        if (error) {
-            response.status(400).send(`error ${error.message}`);
-            return;
-        }
-
-        let total = request.query.total;
-        let eventos = [];
-
-        for (let i = 0; i < total; i++) {
-           eventos[i] = result[i];
-        }
-        response.send({eventos});
-    });
-});
+/* <TERMINA FUNCIONES UTILIES <==== */
 
 
 // ===>> Creamos Endpoint para llamar eventos de nuestra DB
+// ===>> ENDPOINT PARA INDEX
+
+app.get(`/carrusel`, function(request, response) {
+    connection.query("select * from eventos", function(error, result, fields){
+        handleSQLError(response, error, result, function(result){
+            let total = express.request.query.total;
+            let eventos = [];
+
+            for (let i=0; i < total; i++ ) {
+                eventos[i] = result[i];
+            }
+            response.send(eventos);
+        })
+    })
+})
+
 app.get('/eventos/:idEvento', function(request, response) {
-    connection.query(`select * from eventos where id = "${request.params.idEvento}"`, function(error, result, fields) {
-        if (error) {
-            response.status(400).send(`error ${error.message}`);
-            return;
-        }
+    const idEvento = request.params.idEvento;
 
-
-        console.log(request.params.idEvento);
-
-        // let idEvento = request.params.idEvento;
-       if (result.length == 0) {
+    connection.query(`select * from eventos where id = "${idEvento}"`, function(error, result, fields) {
+      handleSQLError(response, error, result, function(result){
+        if (result.length == 0) {
         response.send({});
        } else {
         response.send(result[0]);
-       }
-        
+       } 
+      })
     });
-    
 });
-// Termina Endpoint para llamar eventos <<===
 
 // <<===== --------------------  TERMINA Endpoint para INDEX ----------------------===>>>>>
 
@@ -71,52 +70,62 @@ app.get('/eventos/:idEvento', function(request, response) {
 
 // Creamos Endpoint para login --------------------------------------------------------------------------------------------------------
 
-// Ejemplo como sale la URL en Query String: http://localhost:8000/login?email=luis@email.com&passawod=1234
+// Ejemplo como sale la URL en Query String: http://localhost:8000/login?email=luis@email.com&password=1234
 
 app.get('/login', function(request, response) {
-    connection.query(`select * from usuarios where email = "${request.query.email}" and password = "${request.query.password}"`, function(error, result, fields) {
-    if (error) {
-                response.status(400).send(`error ${error.message}`);
-                return;
-            }
+    const email = request.query.email;
+    const password = request.query.password;
+    const modoNuevo = `select * from usuarios where email = "${email}" and password = "${password}"`;
+
+    console.log(modoNuevo);
+
+    connection.query(`select * from usuarios where email = "${email}" and password = "${password}"`, function(error, result, fields) {
+    handleSQLError(response, error, result, function(result){
+
         if (result.length == 0) {
             response.send({message: "Email o password incorrecto"})
         }  
         else {
             response.send({message: "Login correcto"});
         }
+    })
+        
     }) 
 })
-// Termina Endpoint para login ----------------------------------------------------------------------------------------------------------------
+// Termina ENDPOINT LOGIN <<===-------------------------------------------------------
 
-// Creamos Endpoint para registro ----------------------------------------------------===>>
-app.get('/registro', function(request, response) {
-    connection.query('insert into usuarios values ()', function(error, result, fields) {
-        if(error) {
-            response.status(400).send(`error ${error.message}`);
-            return;
-        }
-        response.send({result});
-    })
-})
-
-// Creamos nuevo Endponint para trabajar con BODY que es tipo POST
+// Creamos ENDPOINT REGISTRO ----------------------------------------------------===>>
 
 app.post('/registro', function(request, response) {
     let nombre = request.body.nombre;
     let apellidos = request.body.apellidos;
     let email = request.body.email;
+    let password = request.body.password;
+    let id = request.body.id;
+
 
     console.log(request.body);
-
-    // Insert into empleados_clientes
     // Insert into usuarios
+        connection.query(`insert into usuarios (email, password) value ("${email}", "${password}")`, function(error, result, fields){
+            handleSQLError(response, error, result, function(result){
+            if (result.length == 0) {
+            response.send({});
+           } else {
+            response.send('Usuario creado correctamente!');
+           }  
+            });
+       
+    });
+    // Insert into empleados_clientes
 
-    response.send({message: "registro"});
 })
 
-// Termina Enpoint para registro -------------------------------------------------------===>>
+// Creamos ENDPOINT REGISTRO ----------------------------------------------------===>>
 
+
+// Termina ENDPOINT REGISTRO <<===---------------------------------------------------
+
+// Termina ENDPOINT REGISTRO <<===---------------------------------------------------
 
 // INICIA LA FUNCION PARA ARRANCAR MYSQL ===>>
 app.listen(8000, function() {       // Indicam en qué puerto lo hemos creados
