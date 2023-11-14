30b243db37750be676f32862b67dcdbcd4a564f1
LuisVenturaH <l_ventur@yahoo.com>
2023-11-14 21:01:47 +0100

    project polo updated

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function handleSQLError(response, error, result, callback) {
 app.get(`/carrusel`, function(request, response) {
     connection.query("select * from eventos", function(error, result, fields){
         handleSQLError(response, error, result, function(result){
-            let total = express.request.query.total;
+            let total = request.query.total;
             let eventos = [];
 
             for (let i=0; i < total; i++ ) {
@@ -96,38 +96,162 @@ app.get('/login', function(request, response) {
 
 // Creamos ENDPOINT REGISTRO ----------------------------------------------------===>>
 
-app.post('/registro', function(request, response) {
+// **************************** INICIO CODIGO CORREGIDO ***************************
+app.post('/registro', function (request, response) {
     let nombre = request.body.nombre;
     let apellidos = request.body.apellidos;
+    let usuario = request.body.usuariosId;
+    let cliente = request.body.clientesId;
+    let telefono = request.body.telefono;
+    let dni = request.body.dni;
     let email = request.body.email;
     let password = request.body.password;
-    let id = request.body.id;
 
+    // Insertar usuario
+    connection.query(`INSERT INTO usuarios (email, password) VALUES (?, ?)`, [email, password], function (error, result, fields) {
+        if (error) {
+            console.error("Error al insertar usuario:", error);
+            response.status(500).send({ message: "Error al registrar usuario" }); // status(500) es un error interno del servidor
+            return;
+        }
 
-    console.log(request.body);
-    // Insert into usuarios
-        connection.query(`insert into usuarios (email, password) value ("${email}", "${password}")`, function(error, result, fields){
-            handleSQLError(response, error, result, function(result){
-            if (result.length == 0) {
-            response.send({});
-           } else {
-            response.send('Usuario creado correctamente!');
-           }  
+        console.log("Usuario insertado correctamente!!!!!");
+
+        // Seleccionar usuario
+        connection.query(`SELECT id FROM usuarios WHERE email = ?`, [email], function (error, result, fields) {
+            if (error) {
+                console.error("Error al seleccionar usuario:", error);
+                response.status(500).send({ message: "Error al seleccionar usuario" });
+                return;
+            }
+
+            let usuarioId = result[0].id;
+            console.log(result[0]);
+
+            // Enlazar empleados_clientes con usuarioId
+            connection.query(`INSERT INTO empleados_clientes (nombre, apellidos, usuarioId, clienteId, telefono, dni) 
+                              VALUES (?, ?, ?, ?, ?, ?)`, [nombre, apellidos, usuarioId, cliente, telefono, dni], function (error, result, fields) {
+                if (error) {
+                    console.error("Error al insertar empleado_cliente:", error);
+                    response.status(500).send({ message: "Error al registrar empleado_cliente" });
+                    return;
+                }
+
+                response.status(200).send({ message: "Registro exitoso" }); // El código 200 indica que ha funcionado todo ok
             });
-       
+        });
     });
-    // Insert into empleados_clientes
+});
 
-})
-
-// Creamos ENDPOINT REGISTRO ----------------------------------------------------===>>
+// ********************* TERMINA CODIGO CORREGIDO *******************************
 
 
 // Termina ENDPOINT REGISTRO <<===---------------------------------------------------
+
+// ******* ==========>>>>> CIERRE ENDPOINT PARA LISTAR CLIENTES
+app.get("/clientes", function(request, response){
+    connection.query(`SELECT * FROM clientes`, function(error, result, fields){
+        handleSQLError(response, error, result, function(result){
+            let clientes = [];
+
+            for (let i=0; i < result.length; i++ ) {
+                clientes[i] = result[i];
+            }
+            response.send(clientes);
+        })
+    })
+})
+
+// ******* ==========>>>>> CIERRE ENDPOINT PARA LISTAR CLIENTES
+
+// ******* ==========>>>>> APERTURA ENDPOINT PARA MODIFICAR DATOS CLIENTES
+                    // ***** RECUERDA QUE CON APP.POST DEBES CAMBIAR EN THUNDER EL TIPO DE BUSQUEDA Y PONER POST
+app.post("/clientes/:id", function(request, response){
+    let razonSocial = request.body.Razon_Social;
+    let cif = request.body.cif;
+    let sector =  request.body.Sector;
+    let telefono = request.body.telefono;
+    let empleados = request.body.Numero_empleados;
+    let clienteId = request.params.id;
+
+    connection.query(`UPDATE clientes SET Razon_Social = "${razonSocial}", CIF = "${cif}", Sector = "${sector}", telefono = "${telefono}", Numero_empleados = "${empleados}" 
+    WHERE id = ${clienteId}`,  function(error, result, fields){
+        if (error) {
+            console.error("Error al modificar cliente:", error);
+            response.status(500).send({ message: "Error al modificar" });
+            return;
+        }
+
+        response.status(200).send({ message: "Cliente actualizado" }); // El código 200 indica que ha funcionado todo ok
+
+        console.log("Usuario actualizado correctamente!!!!!");
+    })
+})
+// ******* ==========>>>>> CIERRE ENDPOINT PARA MODIFICAR CLIENTES
+
+// ******* ============>>>>>>>>>>>>> INSERTAR NUEVOS CLIENTES
+app.post('/clientes', function (request, response) {
+    let nombre = request.body.nombre;
+    let apellidos = request.body.apellidos;
+    let usuario = request.body.usuariosId;
+    let cliente = request.body.clientesId;
+    let telefono = request.body.telefono;
+    let dni = request.body.dni;
+    let email = request.body.email;
+    let password = request.body.password;
+
+    // Insertar usuario ///******* ESTOY JAY QUE MODIFICARLO */
+    connection.query(`INSERT INTO clientes (email, password) VALUES (?, ?)`, [email, password], function (error, result, fields) {
+        if (error) {
+            console.error("Error al insertar usuario:", error);
+            response.status(500).send({ message: "Error al registrar usuario" }); // status(500) es un error interno del servidor
+            return;
+        }
+
+        console.log("Usuario insertado correctamente!!!!!");
+
+        // Seleccionar usuario
+        connection.query(`SELECT id FROM usuarios WHERE email = ?`, [email], function (error, result, fields) {
+            if (error) {
+                console.error("Error al seleccionar usuario:", error);
+                response.status(500).send({ message: "Error al seleccionar usuario" });
+                return;
+            }
+
+            let usuarioId = result[0].id;
+            console.log(result[0]);
+
+            // Enlazar empleados_clientes con usuarioId
+            connection.query(`INSERT INTO empleados_clientes (nombre, apellidos, usuarioId, clienteId, telefono, dni) 
+                              VALUES (?, ?, ?, ?, ?, ?)`, [nombre, apellidos, usuarioId, cliente, telefono, dni], function (error, result, fields) {
+                if (error) {
+                    console.error("Error al insertar empleado_cliente:", error);
+                    response.status(500).send({ message: "Error al registrar empleado_cliente" });
+                    return;
+                }
+
+                response.status(200).send({ message: "Registro exitoso" }); // El código 200 indica que ha funcionado todo ok
+            });
+        });
+    });
+});   //    AQUI TERMINA EL CODIGO QUE HAY QUE MODIFICAR
+
+// app.get("/clientes", function(request, response){
+//     console.log("Insertar clientes")
+// })
+
+// Obtiene datos clientes con el id en :id
+// app.get("/clientes/:id", function(request, response){
+//     console.log("Obtiene datos clientes con el id en :id")
+// })
 
 // Termina ENDPOINT REGISTRO <<===---------------------------------------------------
 
 // INICIA LA FUNCION PARA ARRANCAR MYSQL ===>>
-app.listen(8000, function() {       // Indicam en qué puerto lo hemos creados
+app.listen(8000, function() {       // Indicamos en qué puerto lo hemos creados
     console.log('server up and running!!!')
 });
+
+// Listado de clientes. Con query.
+// Formulario editar
+// Formulario crear
---
+++ public/js/index.js
@@ -0,0 +1,45 @@
+const host = "http://localhost:8000";
+
+window.addEventListener("load", function(event) {
+    // FETCH PARA CAMBIAR EL DOM 
+    fetch(`${host}/carrusel?total=3`)
+    .then(function(response){
+        return response.json();
+    }).then(function(json) {
+        const containerDiv = document.getElementById("container");
+        containerDiv.innerHTML = "<ul>";
+            for (let i = 0; i < json.length; i++) {
+                containerDiv.innerHTML += `<li><button onclick="carruselClick(${json[i].id})">${json[i].nombre}</button> 
+                <p> Haz clic para saber más <------------
+                Fecha inicio: ${json[i].fecha_inicio} Fecha final: ${json[i].fecha_final}</p></li>`;
+            }
+                containerDiv += "</ul>";
+
+                if (json[0].nombre == "onclic") {
+                    containerDiv.innerHTML += `<li>${json[0]} li>`
+                }
+    }).catch(function(error){
+        console.log(error);
+    });
+});
+ // FUNCION QUE PERMITE HACER CLIC A BOTONES Y CAMBIA LISTA DE EVENTOS POR EVENTO UNICO
+function carruselClick(evento_id) {
+    fetch(`${host}/eventos/${evento_id}'`)
+    .then(function(response){
+        return response.json();
+    }).then(function(json) {
+        const containerDiv = document.getElementById("container");
+        containerDiv.innerHTML = 
+        `<img src="/img/${json.nombre}.jpg" width="500px"/>
+        <h1>Bienvenidos a  ${json.nombre} </h1>
+        <h2>El mejor ${json.tipo} de España</h2>
+        <h3>Ven el dia ${json.fecha_inicio} a disfrutar del mejor evento del año</h3>
+        <button class="btn"><a href="http://localhost:8000">Volver</a></button>`; // Evento que regresa a la página inicial
+        
+        
+        console.log(json)
+    }).catch(function(error){
+        console.log(error);
+    }); 
+}
+
